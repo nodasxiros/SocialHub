d3498efe6a2d0c121322af3f454858e3027386f8
Epameinondas Xiros <nodasxiros@users.noreply.github.com>
2016-11-04 17:05:12 +0000

    Update header.php

--- header.php
+++ header.php
@@ -22,20 +22,23 @@ echo "<title>$appname$userstr</title><link rel='stylesheet'" .
       "<script> src='js/bootstrap.min.js'></script>";
 if  ($loggedin)
 	{
-    echo "<br ><ul class='menu'>" .
+    echo "<nav class='navbar navbar-default navbar-fixed-top' role='navigation'>".
+         "<ul class='nav navbar-nav'>".
          "<li><a href='members.php?view=$user'>Home</a></li>" .
          "<li><a href='members.php'>Members</a></li>"         .
          "<li><a href='friends.php'>Friends</a></li>"         .
          "<li><a href='messages.php'>Messages</a></li>"       .
          "<li><a href='profile.php'>Edit Profile</a></li>"    .
-         "<li><a href='logout.php'>Log out</a></li></ul><br>";
+         "<li><a href='logout.php'>Log out</a></li></ul></nav>";
   }
   else
   {
-    echo ("<br><ul class='menu'>" .
+    echo ("<nav class='navbar navbar-default navbar-fixed-top' role='navigation'>".
+    	  "<div class='collapse navbar-collapse' id='navbarCollapse'>" .
+    	  "<ul class='nav navbar-nav'>" .
           "<li><a href='index.php'>Home</a></li>"                .
           "<li><a href='signup.php'>Sign up</a></li>"            .
-          "<li><a href='login.php'>Log in</a></li></ul><br>"     .
+          "<li><a href='login.php'>Log in</a></li></ul></div></nav>"     .
           "<span class='info'>&#8658; You must be logged in to " .
           "view this page.</span><br><br>");
   }
